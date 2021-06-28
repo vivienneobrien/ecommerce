0dfe083efc0f2d783cfe32da5d54979a878ed9d8
vivienneobrien <vxo910@cs.bham.ac.uk>
2021-06-28 17:59:55 +0100

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 
+const HatsPage = () => {
+ <div>
+    <h1>hats page</h1>
+  </div>
+  
+};
+// when here we want to render the page this components loads
 function App() {
   return (
     <div>
-   <HomePage/>
+      <Switch>
+        <Route path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './menu-item.component.scss'
+import {withRouter} from "react-router-dom" // this is a higher order component - take components that returns to us a modified component
 
 
 // doesnt need to hold any state
-const MenuItem = ({ title, imageUrl , size}) => {
+const MenuItem = ({ title, imageUrl , size, history, linkUrl, match}) => {
   return (
-  <div className= {`${size} menu-item`}> 
+  <div className= {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}> 
 
       <div className='background-image'
         style= {{ backgroundImage: `url(${imageUrl})`}}
@@ -18,4 +19,4 @@ const MenuItem = ({ title, imageUrl , size}) => {
   )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem); // with access to locatoin, match and history
